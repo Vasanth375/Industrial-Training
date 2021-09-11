8c33ee41e1cf81af0606be69449b592a5812874b
Vasanth <vasanthnimmala69@outlook.com>
2021-09-11 09:40:21 +0530

    Create Static Method.java

---
+++ Java/Static Method.java
@@ -0,0 +1,22 @@
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	    A();
+        B();
+        C();
+    }
+    static  void A()
+    {
+        System.out.println("A method called");
+    }
+    static void  B()
+    {
+        System.out.println("B method called");
+    }
+    static void C()
+    {
+        System.out.println("C method called");
+    }
+}
