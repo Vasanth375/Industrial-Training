34fd7a3ad72d089f50b27f4f4021688c63aef019
Vasanth <vasanthnimmala69@outlook.com>
2021-10-03 13:10:18 +0530

    Create Exception_without_catch.java

---
+++ Java/Exception_without_catch.java
@@ -0,0 +1,19 @@
+public class Main {
+    static int a;
+    static void show()
+    {
+        try {
+            System.out.println(1/a);
+            System.out.println(1/0);
+        }
+        //finally, block is continued with try without try there's no finally.
+        // finally, block will execute whether exception will be thrown or not
+        finally {
+            System.out.println("your entered into finally block");
+        }
+    }
+    public static void main(String[] args)
+    {
+        show();
+    }
+}
